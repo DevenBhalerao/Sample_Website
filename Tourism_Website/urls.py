1bb5ca2d03ef92088c35e4a4d3e8659a022299bd
DevenBhalerao <devenbhalerao@gmail.com>
2016-03-05 22:15:55 +0530

    added static files

--- Tourism_Website/urls.py
+++ Tourism_Website/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
 ] 
 
 if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_ROOT, document_root=settings.MEDIA_ROOT)
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
